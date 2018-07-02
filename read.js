b3cfe95b258616f9a83c160f86ccf8f01e35353a
donkeykong91 <darylnator@gmail.com>
2018-07-01 18:52:39 -0700

    Asynchronously grabbing each file to check if it's not .DS_Store file nor a directory and displaying it in the command prompt

--- read.js
+++ read.js
@@ -1,9 +1,17 @@
 var fs = require("fs");
+var path = require("path");
 
-fs.readFile("./lib/sayings.md", "UTF-8", function (err, contents) {
-  if (err) {
-    console.log(err);
-  }
+fs.readdir("./lib", function (err, files) {
+  files.forEach(function (filename) {
 
-  console.log(contents);
+    var file = path.join(__dirname, "lib", fileName);
+    var stats = fs.statSync(file);
+
+    if (stats.isFile() && fileName !== ".DS_Store") {
+
+      fs.readFile(file, "UTF-8", function (err, contents) {
+        console.log(contents);
+      });
+    }
+  });
 });
